a0ece1d5f58d48189a95d7908424409dabcf04cf
unoti <unoti2@gmail.com>
2020-01-20 16:48:52 -0800

    Tools: generate asteroid data

--- asteroids/tools.js
+++ asteroids/tools.js
@@ -1,27 +1,58 @@
 import { VectorDisplay } from './vector_display.js';
 
-var display2 = document.getElementById('vector-preview');
-var v = new VectorDisplay(display2);
-var segments = 10;
-let r = 50;
-let angle_delta = Math.PI * 2 / segments;
-let x0 = 0;
-let y0 = 0;
-for (var i = 0; i < segments; i++) {
-    let angle_noise = (Math.random() - 0.5) * 0.5;
-    let radius_noise = 0.4 * r * (Math.random() - 0.5);
-    let a = angle_delta * i + angle_noise;
-    let r1 = r + radius_noise;
-    let x = r1 * Math.cos(a) + 100;
-    let y = r1 * Math.sin(a) + 100;
+var display = new VectorDisplay(document.getElementById('vector-preview'));
+let asteroid = make_asteroid();
+let size = parseInt(document.getElementById('asteroid-size').textContent);
+console.log(`size=${size}`);
+render_asteroid(display, asteroid, size);
+render_asteroid_code(document.getElementById('asteroid-code'), asteroid);
 
-    v.move_to(x, y);
-    if (i == 0) {
-        v.gun(1);
-        x0 = x;
-        y0 = y;
+function make_asteroid() {
+    let segments = 10;
+    let angle_delta = Math.PI * 2 / segments;
+    let points = [];
+    // We'll generate an asteroid of radius 1 here then scale it up before rendering.
+    for (var i = 0; i < segments; i++) {
+        let angle_noise = (Math.random() - 0.5) * 0.5;
+        let radius_noise = 0.2 * (Math.random() - 0.5);
+        let r = 0.5 + radius_noise;
+        let a = angle_delta * i + angle_noise;
+        let x = r * Math.cos(a);
+        let y = r * Math.sin(a);
+    
+        points.push([x,y]);
     }
+    return points;
 }
-v.move_to(x0, y0); // Close it off.
-v.gun(1);
-v.render();
+
+function transform_pt(point, size) {
+    let x = point[0] * size + size;
+    let y = point[1] * size + size;
+    return [x, y];
+}
+
+function render_asteroid(display, points, size) {
+    display.gun(0);
+    let start_pt = transform_pt(points[0], size);
+    display.move_to(start_pt[0], start_pt[1]);
+    display.gun(1);
+    for (let i=1; i < points.length; i++) {
+        let pt = transform_pt(points[i], size);
+        display.move_to(pt[0], pt[1]);
+    }
+    display.move_to(start_pt[0], start_pt[1]);
+    display.gun(0);
+    display.render();
+}
+
+function render_asteroid_code(element, asteroid) {
+    var text = "[\n";
+    for (var i=0; i < asteroid.length; i++) {
+        let x = asteroid[i][0];
+        let y = asteroid[i][1];
+        text += `${x}, ${y},\n`;
+    }
+    text += "],";
+
+    element.textContent = text;
+}
